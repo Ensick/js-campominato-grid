05642303a34fead050ada0eb95138f2bd6052f05
Ensick <fede.censi96@gmail.com>
2022-10-28 14:35:06 +0200

    Aggiunta cartella img con logo Boolean

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,27 +8,33 @@ for (let x = 1; x <= 100; x++) {
     let divNuovo = document.createElement("div")
     divNuovo.classList.add("box-js")
 
+    divNuovo.addEventListener('click' , function(){
+        console.log(this);
+        this.classList.toggle('ms-color-custom')
+
+    })
+
 
     if (x % 3 == 0 && x % 5 == 0) {
 
         divNuovo.innerHTML = `<h4>${x}</h4>`;
-        divNuovo.classList.add()
+        
 
     } else if (x % 5 == 0) {
 
         divNuovo.innerHTML = `<h4>${x}</h4>`;
-        divNuovo.classList.add()
+        
         
 
     } else if (x % 3 == 0) {
 
         divNuovo.innerHTML = `<h4>${x}</h4>`;
-        divNuovo.classList.add()
+        
         
     } else {
 
         divNuovo.innerHTML = `<h4>${x}</h4>`;
-        divNuovo.classList.add()
+        
         
     }
 
